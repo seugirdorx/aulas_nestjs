bb1daaf72fb4843c43d9398c79357f62df10882a
seugirdorx <amandagetion@gmail.com>
2022-10-11 09:42:58 -0300

    BlogPessoal_1010

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Postagem } from './postagem/entities/postagem.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'db_blog_pessoal',
+    entities: [Postagem],
+    synchronize: true
+    })
+  ],
+    controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,21 @@
+import { IsNotEmpty, MaxLength } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity ({name: "tb_postagens"})
+export class Postagem{
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @IsNotEmpty()
+    @MaxLength(100)
+    @Column({length: 100, nullable: false})
+    titulo: string
+
+    @IsNotEmpty()
+    @Column({length: 1000, nullable: false})
+    texto: string
+
+    @IsNotEmpty()
+    @UpdateDateColumn()
+    data: Date 
+}
